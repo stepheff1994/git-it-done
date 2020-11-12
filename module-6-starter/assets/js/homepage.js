f8733bc8c43cb39153bbd7f02bf08940f7d70dcc
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2020-11-12 17:40:46 -0500

    form input to variable for function

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -1,5 +1,34 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  console.log(event);
+};
+
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
